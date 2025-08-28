1c0f3d639437c68d177f8e074dc974ab8fa15365
kitzki <devenchy.saintpreux@gmail.com>
2025-08-28 20:48:18 +0000

    Finished Easy gcc Level

---
+++ c-programs/hello_world.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    printf("Hello, C!\n");
+    int num = 5;
+    printf("Number: %d\n", num);
+
+    int age;
+    scanf("%d", &age);
+    printf("Age: %d\n", age);
+
+    return 0;
+}
+
